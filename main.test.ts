4d56ee8bba88577191c8232cd4e8501b26fb0e14
Paul Shippy <tpaul@shippy.com>
2019-09-02 13:41:48 -0700

    test some more complete game scenarios

--- main.test.ts
+++ main.test.ts
@@ -6,12 +6,88 @@ it("scores perfect game", () => {
     // WHEN
     // Bowl twelve strikes
     for (let i = 0; i < 12; i++) {
-        game.bowl('X');
+        game.hit(10);
     }
 
     // THEN
     // Expect score of 300
     const score = game.score();
     expect(score).toEqual(300);
+
+});
+it("scores all gutter balls", () => {
+    // GIVEN
+    // Initialize game
+    const game = new BowlingGame();
+
+    // WHEN
+    // Bowl twenty gutter balls
+    for (let i = 0; i < 20; i++) {
+        game.hit(0);
+    }
+
+    // THEN
+    // Expect score of 0
+    const score = game.score();
+    expect(score).toEqual(0);
+
+});
+it("scores all nines and spares", () => {
+    // GIVEN
+    // Initialize game
+    const game = new BowlingGame();
+
+    // WHEN
+    // Bowl ten spares
+    for (let i = 0; i < 10; i++) {
+        game.hit(9);
+        game.hit(1);
+    }
+    // Bowl extra shot from 10th frame
+    game.hit(9);
+
+    // THEN
+    // Expect score of 19 * 10 = 190
+    const score = game.score();
+    expect(score).toEqual(190);
+
+});
+
+it("scores all nines", () => {
+    // GIVEN
+    // Initialize game
+    const game = new BowlingGame();
+
+    // WHEN
+    // Bowl ten nines
+    for (let i = 0; i < 10; i++) {
+        game.hit(9);
+        game.hit(0); // second ball is gutter ball
+    }
+
+    // THEN
+    // Expect score of 9 * 10 = 90
+    const score = game.score();
+    expect(score).toEqual(90);
+
+});
+
+
+it("scores 0 - 10", () => {
+    // GIVEN
+    // Initialize game
+    const game = new BowlingGame();
+
+    // WHEN
+    // Bowl ten times starting with a gutter ball and then each time hitting one more ball
+    for (let i = 0; i < 10; i++) {
+        game.hit(i);
+        game.hit(0); // second ball is gutter ball
+    }
+
+    // THEN
+    // Expect score of 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9
+    const score = game.score();
+    expect(score).toEqual(0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
 
 });
